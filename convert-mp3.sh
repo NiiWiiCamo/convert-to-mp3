ec66bfb4327b9123b4175134d3275bdedd0194ca
NiiWiiCamo <mail@nilsblume.de>
2023-09-21 15:11:49 +0200

    debug

--- convert-mp3.sh
+++ convert-mp3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 convert_mp3() {
-    ffmpeg -hide_banner -nostats -loglevel panic -i -vn -y /watch/$file /output/${file%.*}.mp3 &
+    ffmpeg -hide_banner -nostats -i -vn -y /watch/$file /output/${file%.*}.mp3 &
     echo "converting $file"
 }
 
 while true; do
-    inotifywait -t 5 -m /watch -e create -e moved_to | while read path action file
+    inotifywait -t 5 -m /watch -e create -e moved_to -e modify | while read path action file
     do
         if [ ${file##*.} == "mp4" ]
         then
