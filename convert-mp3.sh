2e5402d5ddcf009f844594bd9e00183c3710ec49
NiiWiiCamo <mail@nilsblume.de>
2023-09-21 15:14:15 +0200

    .

--- convert-mp3.sh
+++ convert-mp3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 convert_mp3() {
-    ffmpeg -hide_banner -nostats -i -vn -y /watch/$file /output/${file%.*}.mp3 &
+    ffmpeg -hide_banner -nostats -loglevel panic -vn -y -i /watch/$file /output/${file%.*}.mp3 &
     echo "converting $file"
 }
 
