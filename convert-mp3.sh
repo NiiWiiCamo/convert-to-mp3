a18c75cecb39ba6554a420f7df6124d3119e9cc2
NiiWiiCamo <mail@nilsblume.de>
2023-09-21 14:10:31 +0200

    initial commit.

---
+++ convert-mp3.sh
@@ -0,0 +1,13 @@
+convert_mp3() {
+ffmpeg -hide_banner -nostats -loglevel panic -i -vn -y /watch/$file /output/${file%.*}.mp3 &
+echo "converting $file"
+}
+
+while true; do
+    inotifywait -m ~/watch -e create -e moved_to | while read path action file; do
+    if [ ${file##*.} != "mp3" ]
+    then
+        convert_mp3 && rm /watch/$file
+    fi
+    done
+done
