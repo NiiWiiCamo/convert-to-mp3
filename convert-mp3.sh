b4b3edac6ab99cd1b321b98344907b9940405ec0
NiiWiiCamo <mail@nilsblume.de>
2023-09-21 14:56:21 +0200

    quiet

--- convert-mp3.sh
+++ convert-mp3.sh
@@ -6,7 +6,7 @@ convert_mp3() {
 }
 
 while true; do
-    inotifywait -t 5 -m /watch/*.mp4 -e create -e moved_to | while read path action file
+    inotifywait -t 5 -q -m /watch/*.mp4 -e create -e moved_to | while read path action file
     do
         if [ ${file##*.} != "mp3" ]
         then
