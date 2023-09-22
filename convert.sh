3fc405164e914d354f2189cfc808a6922732cb36
NiiWiiCamo <mail@nilsblume.de>
2023-09-22 09:53:44 +0200

    1.2

--- convert.sh
+++ convert.sh
@@ -24,6 +24,7 @@ while $true:
 do
   for file in $(find ${WATCHFOLDER} -name "*.${FORMAT_IN}")
   do
+    echo "${file}"
     if [[ ${file##*.} == "${FORMAT_IN}" ]]
     then
       convert_format && \
